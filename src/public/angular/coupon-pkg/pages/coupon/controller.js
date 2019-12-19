74d5438cdca7ec1e98b52374d623eebcd0c78a0a
Vijay S <vijay@uitoux.in>
2019-12-19 14:41:20 +0530

    updates

--- src/public/angular/coupon-pkg/pages/coupon/controller.js
+++ src/public/angular/coupon-pkg/pages/coupon/controller.js
@@ -4,6 +4,7 @@ app.component('couponCodes', {
         $scope.loading = true;
         var self = this;
         self.hasPermission = HelperService.hasPermission;
+        self.import_permission = self.hasPermission('import-coupon');
         var table = $('#coupon_code_table').DataTable({
             "dom": dom_structure,
             info: true,
@@ -41,7 +42,7 @@ app.component('couponCodes', {
                 { data: 'uploaded_date', name: 'coupons.created_at', searchable: false },
             ],
             "infoCallback": function(settings, start, end, max, total, pre) {
-                $('#table_info').html(max)
+                $('#table_info').html(total)
             },
             "initComplete": function(settings, json) {
                 $('.dataTables_length select').select2();
@@ -54,10 +55,12 @@ app.component('couponCodes', {
         $('.page-header-content .display-inline-block .data-table-title').html('Coupon Codes <span class="badge badge-secondary" id="table_info">0</span>');
         $('.page-header-content .search.display-inline-block .add_close_button').html('<button type="button" class="btn btn-img btn-add-close"><img src="' + image_scr2 + '" class="img-responsive"></button>');
         $('.page-header-content .refresh.display-inline-block').html('<button type="button" class="btn btn-refresh"><img src="' + image_scr3 + '" class="img-responsive"></button>');
-
-        var addnew_block = $('#add_new_wrap').html();
-        $('.page-header-content .alignment-right .add_new_button').html('<a role="button" id="open" data-toggle="modal"  data-target="#coupon-code-filter" class="btn btn-img"> <img src="' + image_scr + '" alt="Filter" onmouseover="this.src=' + image_scr1 + '" onmouseout="this.src=' + image_scr + '"></a>' +
-            addnew_block);
+        if (self.import_permission == true) {
+            var addnew_block = $('#add_new_wrap').html();
+        } else {
+            var addnew_block = '';
+        }
+        $('.page-header-content .alignment-right .add_new_button').html('<a role="button" id="open" data-toggle="modal"  data-target="#coupon-code-filter" class="btn btn-img"> <img src="' + image_scr + '" alt="Filter" onmouseover="this.src=' + image_scr1 + '" onmouseout="this.src=' + image_scr + '"></a>' + addnew_block);
 
         $('.btn-add-close').on("click", function() {
             $('#coupon_code_table').DataTable().search('').draw();
