f7ea4a351fdc7037ee28f00faa4042277735ad1e
Abdul <abdul@uitoux.in>
2020-01-13 11:52:57 +0530

    u

--- src/CouponPkgServiceProvider.php
+++ src/CouponPkgServiceProvider.php
@@ -14,6 +14,7 @@ class CouponPkgServiceProvider extends ServiceProvider {
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 		$this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 		$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+		$this->loadViewsFrom(__DIR__ . '/views', 'coupon-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
 		]);
--- src/public/angular/coupon-pkg/pages/coupon/controller.js
+++ src/public/angular/coupon-pkg/pages/coupon/controller.js
@@ -1,3 +1,27 @@
+app.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider.
+    //Coupon
+    when('/coupon-pkg/coupons', {
+        template: '<coupon-codes></coupon-codes>',
+        title: 'Coupon Codes',
+    }).
+    when('/coupon-pkg/coupon/view/:date', {
+        template: '<coupon-codes-view></coupon-codes-view>',
+        title: 'Coupon Code View',
+    }).
+    when('/coupon-pkg/coupon/view/:date/:id', {
+        template: '<coupon-view></coupon-view>',
+        title: 'Coupon Code View',
+    }).
+    when('/coupon-pkg/claimed-coupons', {
+        template: '<claimed-coupons></claimed-coupons>',
+        title: 'Claimed Coupons',
+    });
+
+
+}]);
+
 app.component('couponCodes', {
     templateUrl: coupon_codes_list_template_url,
     controller: function($http, $location, HelperService, $scope, $routeParams, $rootScope, $location) {
